bbc6b04a9f1b67af163dc6f3d1992d4ab843941b
edi vidrascu <edividrascu@gmail.com>
2023-11-04 00:49:38 +0100

    HASTA AQUI BIEN EL SLIDE 1.2

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 new Glider(document.querySelector('.glider'), {
-  slidesToScroll: 1,
-  slidesToShow: 5.5,
-  draggable: true,
-  dots: '.dots',
+  slidesToShow: 3, // Cantidad de diapositivas visibles
+  slidesToScroll: 1, // Cantidad de diapositivas para desplazarse
+  itemWidth: 300, // Ancho de cada diapositiva
+  draggable: true, // Permitir arrastrar
+  dots: '.dots', // Selector de puntos
   arrows: {
     prev: '.glider-prev',
     next: '.glider-next'
-  }
+  },
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });

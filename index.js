98fbd63ef28314ced7eee14e9ced7ee5bab2c246
edi vidrascu <edividrascu@gmail.com>
2023-11-04 20:34:19 +0100

    plantilla card 1.0

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ new Glider(document.querySelector('.glider'), {
   slidesToShow: 3, // Cantidad de diapositivas visibles
   slidesToScroll: 1, // Cantidad de diapositivas para desplazarse
   itemWidth: 300, // Ancho de cada diapositiva
-  draggable: true, // Permitir arrastrar
-  dots: '.dots', // Selector de puntos
+  draggable: false, // Permitir arrastrar
+  dots: '', // Selector de puntos
   arrows: {
     prev: '.glider-prev',
     next: '.glider-next'

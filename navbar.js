8a0c1ce403cf60e401137889fac5b4d0136243d8
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-08 10:16:50 +0100

    Cambios en la sección header y featured

--- navbar.js
+++ navbar.js
@@ -1,13 +1,13 @@
 /* Dropdown-menu*/
 let body = document.getElementsByTagName("body");
+let selectOptions = document.getElementsByClassName("select");
 let options = document.getElementsByClassName("options");
+let arrow = selectOptions[0].getElementsByClassName("d-arrow");
 
 body[0].addEventListener("click", function() {
     options[0].style = "visibility:hidden"
+    arrow[0].style = "transform: rotate(0deg); transition-duration: 200ms;"
   });
-
-let selectOptions = document.getElementsByClassName("select");
-let arrow = selectOptions[0].getElementsByClassName("d-arrow");
 
 selectOptions[0].addEventListener("click", function(e){
     e.stopPropagation();

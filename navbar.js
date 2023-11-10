0453bd29bb348b37b746633526e48de7e4911dff
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-10 20:15:47 +0100

    Botones arreglados

--- navbar.js
+++ navbar.js
@@ -4,11 +4,6 @@ let selectOptions = document.getElementsByClassName("select");
 let options = document.getElementsByClassName("options");
 let arrow = selectOptions[0].getElementsByClassName("d-arrow");
 
-body[0].addEventListener("click", function() {
-    options[0].style = "visibility:hidden"
-    arrow[0].style = "transform: initial; transition-duration: 200ms;"
-  });
-
 selectOptions[0].addEventListener("click", function(e){
     e.stopPropagation();
     let status = getComputedStyle(options[0]);
@@ -28,23 +23,28 @@ let buttons = navButtons[0].getElementsByTagName("button");
 
 for(i=0;i<buttons.length;i++){ 
   buttons[i].addEventListener("click", function(e){
-
+    options[0].style = "visibility:hidden"
     e.stopPropagation();
     let arrows = document.getElementsByClassName("d-arrow");
     for(j = 0;j<arrows.length;j++){
       arrows[j].style= "transform: initial; transition-duration: 200ms;"
     }   
-    arrow = this.getElementsByClassName("d-arrow");
-    arrowStatus = getComputedStyle(arrow[0],null);
+    arrowButtons = this.getElementsByClassName("d-arrow");
+    arrowStatus = getComputedStyle(arrowButtons[0],null);
     if(arrowStatus.transform === 'matrix(-1, 0, 0, -1, 0, 0,)'|| arrowStatus.transform ==='none'){
-      arrow[0].style= "transform: rotate(180deg); transition-duration: 200ms;"
+      arrowButtons[0].style= "transform: rotate(180deg); transition-duration: 200ms;"
     }
     else{
-      arrow[0].style= "transform: initial; transition-duration: 200ms;"  
+      arrowButtons[0].style= "transform: initial; transition-duration: 200ms;"  
     }
   });
 }
 
+body[0].addEventListener("click", function() {
+  options[0].style = "visibility:hidden"
+  arrow[0].style = "transform: initial; transition-duration: 200ms;"
+  arrowButtons[0].style = "transform: initial; transition-duration: 200ms;"
+});
 
 
 

1cc1f3adc587327a67b1bbf8c3243c781a14738f
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-10 20:29:50 +0100

    botones corregidos bug

--- navbar.js
+++ navbar.js
@@ -4,18 +4,6 @@ let selectOptions = document.getElementsByClassName("select");
 let options = document.getElementsByClassName("options");
 let arrow = selectOptions[0].getElementsByClassName("d-arrow");
 
-selectOptions[0].addEventListener("click", function(e){
-    e.stopPropagation();
-    let status = getComputedStyle(options[0]);
-    if( status.visibility == "visible"){
-        options[0].style = "visibility:hidden;"
-        arrow[0].style = "transform: rotate(0deg); transition-duration: 200ms;"
-    }else{
-        options[0].style = "visibility:visible;"
-        arrow[0].style = "transform: rotate(180deg); transition-duration: 200ms;"
-    }  
-  });
-
 /*Rotación arrows */
 
 let navButtons = document.getElementsByClassName("nav-buttons");
@@ -39,6 +27,19 @@ for(i=0;i<buttons.length;i++){
     }
   });
 }
+
+selectOptions[0].addEventListener("click", function(e){
+  e.stopPropagation();
+  let status = getComputedStyle(options[0]);
+  if( status.visibility == "visible"){
+      options[0].style = "visibility:hidden;"
+      arrow[0].style = "transform: rotate(0deg); transition-duration: 200ms;"
+  }else{
+      options[0].style = "visibility:visible;"
+      arrow[0].style = "transform: rotate(180deg); transition-duration: 200ms;"
+      arrowButtons[0].style = "transform: initial; transition-duration: 200ms;"
+  }  
+});
 
 body[0].addEventListener("click", function() {
   options[0].style = "visibility:hidden"

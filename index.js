5db5884e561421f27f572814f68942bfc81155b1
edi vidrascu <edividrascu@gmail.com>
2023-11-13 16:30:25 +0100

    guardado antes del cambio

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ document.addEventListener('DOMContentLoaded', function () {
       slidesToShow: 10,
       slidesToScroll: 5,
       draggable: false,
-      dots: '.dots',  // Corregir aquí
+      dots: '.dot',  // Corregir aquí
       arrows: {
         prev: '.glider-prev',
         next: '.glider-next'
-      }
+      },
+      itemWidth: 268,
     });
     
     
   
-    glider.scroll((currentSlide) => {
-      const prevButton = document.querySelector('.glider-prev');
-      const nextButton = document.querySelector('.glider-next');
-      
-    });glider.refresh(true);
-  });
-
+    glider.on('glider-slide-hidden', (event, index) => {
+      const totalSlides = glider.slides.length;
   
+      // Verifica si la siguiente diapositiva está fuera de la vista (última diapositiva)
+      if (index === totalSlides - 2) {
+        // Muestra un alert con el total de diapositivas
+        alert(`¡Has llegado a la última diapositiva! Total de diapositivas: ${totalSlides}`);
+        // Puedes agregar aquí cualquier acción adicional que desees realizar
+      }
+    });
+  
+    glider.refresh(true);
+  });
+  
+

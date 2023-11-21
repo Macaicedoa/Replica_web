4727d66ad4058cbf8f9fa6c3b68f99f74dfb2e11
edi vidrascu <edividrascu@gmail.com>
2023-11-21 12:57:30 +0100

    mas o menos falta un poco de media debajo de 500px

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
   var glide = new Glide('.glide', {
-    type: 'carousel',
+    type: 'slider',
     perView: 9,
     focusAt: 0,
+    
+    startAt: 0,
     bound: true,
+    rewind: false,
     breakpoints: {
-      1000: {
-        perView: 3
-      },
-      480: {
-        perView: 2
-      }
+      2500: { perView: 9 },
+      2400: { perView: 8 },
+      2100: { perView: 7 },
+      1800: { perView: 6 },
+      1500: { perView: 5 },
+      1300: { perView: 4 },
+      1000: { perView: 3 },
+      850:  { perView: 2 },
+      550:  { perView: 1 },
+      450:  { perView: 1 }
     }
   });
 
   glide.on('run.after', function () {
-    var isEnd = glide.index >= glide.length - glide.settings.perView;
+    var isEnd = glide.index === 2;
     var isFirst = glide.index === 0;
     
     var nextButton = document.querySelector('.glide__arrow--right');
@@ -24,21 +31,29 @@ document.addEventListener('DOMContentLoaded', function () {
     if (nextButton) {
       if (isEnd) {
         nextButton.classList.add('hidden');
-        // Alert cuando llega a la última tarjeta
-        alert('¡Estás en la última tarjeta!');
+        
+        
       } else {
         nextButton.classList.remove('hidden');
+       
       }
     }
 
     if (prevButton) {
       if (isFirst) {
         prevButton.classList.add('hidden');
+        
       } else {
         prevButton.classList.remove('hidden');
+        
       }
     }
   });
+
+  var prevButton = document.querySelector('.glide__arrow--left');
+  if (prevButton) {
+    prevButton.classList.add('hidden');
+  }
 
   glide.mount();
 });
